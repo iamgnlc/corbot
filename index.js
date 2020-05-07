46cab78ac00e526f3abcb8c6345701608d00268d
GNLC <gianluca.agnocchetti@gmail.com>
2020-05-07 09:07:04 +0100

    1st commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+const TelegramBot = require("node-telegram-bot-api");
+
+const token = process.env.BOT_TOKEN;
+
+const bot = new TelegramBot(token, { polling: true });
+
+const dictionary = require("./dictionary");
+
+const getRandoQuote = (match) => {
+  return dictionary[Math.floor(Math.random() * dictionary.length)];
+};
+
+bot.onText(/(o co)/i, (msg, match) => {
+  const chatId = msg.chat.id;
+
+  console.log(msg);
+
+  bot.sendMessage(chatId, getRandoQuote(), { parse_mode: "Markdown" });
+});
